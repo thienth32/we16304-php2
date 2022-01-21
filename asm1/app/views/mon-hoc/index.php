9d69be7972208b318747c947358381a50de75107
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-01-21 20:15:21 +0700

    Update index.php

--- asm1/app/views/mon-hoc/index.php
+++ asm1/app/views/mon-hoc/index.php
@@ -10,7 +10,9 @@
         <?php foreach($subjects as $sub): ?>
             <tr>
                 <td><?= $sub->id ?></td>
-                <td><?= $sub->name ?></td>
+                <td>
+                    <a href="<?= BASE_URL . 'quiz?subjectId=' . $sub->id?>"><?= $sub->name ?></a>
+                </td>
                 <td>
                     <a href="<?= BASE_URL . 'mon-hoc/cap-nhat?id=' . $sub->id ?>">Sửa</a>
                     <a href="<?= BASE_URL . 'mon-hoc/xoa?id=' . $sub->id ?>">Xóa</a>
